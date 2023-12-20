1fc4b4f1cc5d55b8cbfcbaaef93822def1e256a6
saverioannpy <saverio.annunziata@outlook.com>
2023-12-20 11:51:30 +0100

    feat: scaffoldinf

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default{
+  name: 'App',
+  component: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
